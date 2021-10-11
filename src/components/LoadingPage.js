82834883862ec0b03599a03255d381057bfeb8b1
misz0rr <maciejmyszka99@gmail.com>
2021-10-11 20:27:44 +0200

    adding logo to loading page

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,11 +1,21 @@
 import React from "react";
+import logo from "../images/starwars_logo.png";
+import logo_mobile from "../images/starwars_logo-mobile.png";
 
 const LoadingPage = () => {
   return (
     <div className="loading-wrapper">
-      <h2>Loading data<div className="loader"></div></h2>
+      <img
+        src={logo_mobile}
+        alt="STAR WARS CHARACTERS LIBRARY"
+        className="mobile"
+      />
+      <img src={logo} alt="STAR WARS CHARACTERS LIBRARY" className="logo" />
+      <h2>
+          Loading data<div className="loader"></div>
+        </h2>
     </div>
-  )
-}
+  );
+};
 
 export default LoadingPage;
